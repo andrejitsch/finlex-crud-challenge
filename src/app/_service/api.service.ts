f578e2974b37f6a93532cf64ae8d8b3cff890d2f
andrej <ad@toget.hr>
2020-10-12 19:30:45 +0200

    create CRUD

--- src/app/_service/api.service.ts
+++ src/app/_service/api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllData(): Observable<any> {
+    return this.http.get<any[]>('http://dummy.restapiexample.com/api/v1/employees');
+  }
+
+  addData(object): Observable<any> {
+    return this.http.post<any[]>('http://dummy.restapiexample.com/api/v1/create', object);
+  }
+
+  deleteData(objectID): Observable<any> {
+    return this.http.delete<any[]>('http://dummy.restapiexample.com/api/v1/delete/' + objectID.toString());
+  }
+
+  updateEmployee(objectID, object): Observable<any> {
+    return this.http.put<any[]>('http://dummy.restapiexample.com/api/v1/update/' + objectID, object);
+  }
 }
