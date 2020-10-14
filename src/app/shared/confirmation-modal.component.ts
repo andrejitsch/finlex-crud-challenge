adec1410989f7f024a1d3ed60e38f9c1f0ce8b33
andrej <ad@toget.hr>
2020-10-14 23:14:14 +0200

    notification confirm

--- src/app/shared/confirmation-modal.component.ts
+++ src/app/shared/confirmation-modal.component.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   template: `
-    <div class="modal-dialog" aria-hidden="true">
+    <div class="modal-dialog modal-notify modal-danger" aria-hidden="true">
       <div class="modal-content">
-        <div class="modal-header">
+        <div class="modal-header danger" fxLayoutAlign="center center">
           <h4><span>Are you sure?</span></h4>
         </div>
-        <div class="modal-body" fxLayout="center center">
+        <div class="modal-body" fxLayoutAlign="center center">
           <mdb-icon fas icon="times" size="4x" class="animated rotateIn"></mdb-icon>
         </div>
-        <div class="modal-footer">blah</div>
+        <div class="modal-footer" fxLayoutAlign="center center">
+          <button mdbBtn color="danger" outline="true" rounded="true">No</button>
+          <button mdbBtn color="danger" rounded="true">Yes</button>
+        </div>
       </div>
     </div>
   `,
