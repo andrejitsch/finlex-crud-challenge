3e92b23b46947ce79664b2eb645c07bce4b57c46
andrej <ad@toget.hr>
2020-10-16 12:42:19 +0200

    adde from mdb-bootstrap a basic pie chart

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -18,6 +18,25 @@ export class EmployeeListComponent implements OnInit, AfterViewInit {
   employeeList: Employee[] = [];
   previous: any = [];
   headElements = ['ID', 'Name', 'Age', 'Salary'];
+  public chartType = 'pie';
+
+  public chartDatasets: Array<any> = [
+    { data: [300, 50, 100, 40, 120], label: 'My First dataset' }
+  ];
+
+  public chartLabels: Array<any> = ['Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774'],
+      borderWidth: 2,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true
+  };
 
   textSearch = '';
   modalRf: MDBModalRef;
@@ -131,4 +150,7 @@ export class EmployeeListComponent implements OnInit, AfterViewInit {
       this.mdbTable.setDataSource(prev);
     }
   }
+
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
 }
