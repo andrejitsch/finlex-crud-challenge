de900c0ac227e7f276e8374f8be8a950145677d4
andrej <ad@toget.hr>
2020-10-28 17:49:04 +0100

    add child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {EmployeeListComponent} from './employee-list/employee-list.component';
+import {EmployeeCardComponent} from './employee-list/employee-card/employee-card.component';
 
 const routes: Routes = [
 
   {
     path: '',
-    component: EmployeeListComponent
+    component: EmployeeListComponent,
+    children: [{
+      path: ':id',
+      component: EmployeeCardComponent
+    }]
   }
 ];
 
